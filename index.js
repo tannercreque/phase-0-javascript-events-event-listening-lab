ae2f0a45262bea4b133730520fa0cb6e997c853e
Tanner Creque <tanner.creque@gmail.com>
2021-03-07 11:28:07 -0700

    Finished Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-function addingEventListener() {
-}
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace ("px", "")
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0); {
+    dodger.style.left = `${left - 1}px`;
+
+  }
+};
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
